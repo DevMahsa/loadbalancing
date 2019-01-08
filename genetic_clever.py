7f49fe5c53a3e2a4d660af1347d576f0e0853e88
=Mahsa <mahsa.gol89@gmail.com>
2019-01-08 22:58:38 +0330

    '6th'

--- genetic_clever.py
+++ genetic_clever.py
@@ -1,5 +1,4 @@
-from random import sample, random
-import numpy as np
+from random import random
 from numpy.core.tests.test_mem_overlap import xrange
 from random import randint
 
@@ -16,25 +15,33 @@ def objective_function(vector):
 
 
 def random_bitstring(num_bits):
-    return map(lambda x: iif(x < 50, '1', '0'), sample(range(100), num_bits))
+    bitstring = ""
+    for i in [randint(0, 1) for x in range(num_bits)]:
+        bitstring += str(i)
+    return bitstring
+
+    # return map(lambda x: iif(x < 50, '1', '0'), sample(range(100), num_bits))
+
 
 def decode(bitstring, search_space, bits_per_param):
     vector = []
     for i, bounds in enumerate(search_space):
-        sum = i * bits_per_param
-        bitstring = bitstring.__sizeof__()
-        param = np.zeros(bitstring, str)
-        param += [i for i in xrange(bits_per_param)]
-    for j in xrange(0, len(param)):
-        sum += iif(param[j] == '1', '0', '1') * (2 ** j), param[j]
-    min, max = bounds
-    vector = min + ((max - min) / ((2.0 ** bits_per_param) - 1.0)) * sum
+        off, sum = i * bits_per_param, 0.0
+        param = (bitstring[off:(off + bits_per_param)])[::-1]
+        for j in xrange(0, len(param)):
+            if param[j].__str__() == '1':
+                sum += 1.0 * (2 ** float(j))
+            else:
+                sum += 0.0 * (2 ** float(j))
+        min, max = bounds
+        vector.append(min + ((max - min) / ((2.0 ** bits_per_param) - 1.0)) * sum)
     return vector
 
 
 def fitness(candidate, search_space, param_bits):
-    vector = decode(candidate['bitstring'], search_space, param_bits)
-    fitness = objective_function(vector)
+    candidate['vector'] = decode(candidate['bitstring'], search_space, param_bits)
+    candidate['fitness'] = objective_function(candidate['vector'])
+    return candidate
 
 
 def binary_tournament(pop):
@@ -59,9 +66,9 @@ def crossover(parent1, parent2, rate):
     child = ""
     for i in xrange(0, len(parent1)):
         if random() < 0.5:
-            child = parent1[i]
+            child += parent1[i]
         else:
-            child = parent2[i]
+            child += parent2[i]
     return child
 
 
@@ -86,9 +93,9 @@ def bitclimber(child, search_space, p_mut, max_local_gens, bits_per_param):
     current = child
     for _ in range(max_local_gens):
         candidate = {}
-        candidate[:'bitstring'] = point_mutation(current[:'bitstring'], p_mut)
-        fitness(candidate, search_space, bits_per_param)
-        if candidate[:'fitness'] <= current[:'fitness']:
+        candidate['bitstring'] = point_mutation(current['bitstring'], p_mut)
+        current = fitness(candidate, search_space, bits_per_param)
+        if candidate['fitness'] <= current['fitness']:
             current = candidate
     return current
 
@@ -96,29 +103,34 @@ def bitclimber(child, search_space, p_mut, max_local_gens, bits_per_param):
 def search(max_gens, search_space, pop_size, p_cross, p_mut, max_local_gens, p_local, bits_per_param=16):
     pop = []
     for i in xrange(0, pop_size):
-        pop.append({'bitstring': random_bitstring(len(search_space)*bits_per_param)})
-    for c in pop:
-        c = fitness(c, search_space, bits_per_param)
+        pop.append({'bitstring': random_bitstring(len(search_space) * bits_per_param)})
+        fitness(pop[i], search_space, bits_per_param)
     pop.sort(key=lambda x: x['fitness'])
-    gen, best = 0,pop[0]
+    gen, best = 0, pop[0]
     for gen in xrange(max_gens):
         selected = [binary_tournament(pop) for i in xrange(0, pop_size)]
         children = reproduce(selected, pop_size, p_cross, p_mut)
-        for child in children:
-            if random() < p_local:
-                child = bitclimber(child, search_space, p_mut, max_local_gens,
-                                   bits_per_param)
-            pop = child
+        pop = create_populations(bits_per_param, children, max_local_gens, p_mut, pop, search_space, p_local)
         pop.sort(key=lambda x: x['fitness'])
         if pop[0]['fitness'] > best['fitness']:
             best = pop[0]
-        print
-        (" > gen %d, best: %d, %s" % (gen, best['fitness'], best['bitstring']))
+        print(" > gen %d, best: fitness, best: bitstring: %s, %s" % (gen, best['fitness'], best['bitstring']))
+
     return best
+
+
+def create_populations(bits_per_param, children, max_local_gens, p_mut, pop, search_space, p_local):
+    for child in children:
+        if random() < p_local:
+            pop.append(bitclimber(child, search_space, p_mut, max_local_gens,
+                                  bits_per_param))
+    return pop
+
 
 def main():
     # problem configuration
-    problem_size = [1,2,5]
+    problem_size = 3
+    search_space = [[-5, +5] for y in range(problem_size)]
     # algorithm configuration
     max_gens = 100
     pop_size = 100
@@ -126,10 +138,10 @@ def main():
     p_mut = 1.0 / (3 * 16)
     max_local_gens = 20
     p_local = 0.5
-    search_space='3'
     # execute the algorithm
     best = search(max_gens, search_space, pop_size, p_cross, p_mut, max_local_gens, p_local)
-    print ("done! Solution: f=%f,s=%s,v=%v " % (best['fitness'], str(best['bitstring']), str(best['vector'].getmembers())))
+    print(
+        "done! Solution: f=%s,s=%s,v=%s " % (best['fitness'], str(best['bitstring']), str(best['vector'])))
 
 
 if __name__ == "__main__":
